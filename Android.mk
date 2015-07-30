74c74491b251a1048164f6214db8f79b26db362e
Filipe Brandenburger <filbranden@google.com>
2015-07-30 14:47:08 -0700

    iw: fix references to libnl in Android.mk
    The latest AOSP refers to that library as libnl and not libnl_2.
    
    TEST=Built AOSP tree with this patchset, tested the generated iw binary.
    
    Signed-off-by: Filipe Brandenburger <filbranden@google.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- Android.mk
+++ Android.mk
@@ -9,15 +9,11 @@ include $(LOCAL_PATH)/Makefile
 
 LOCAL_SRC_FILES := $(patsubst %.o,%.c,$(OBJS)) android-nl.c
 
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH) \
-	external/libnl-headers/
-
 LOCAL_CFLAGS += -DCONFIG_LIBNL20
 LOCAL_LDFLAGS := -Wl,--no-gc-sections
 #LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_TAGS := eng
-LOCAL_STATIC_LIBRARIES := libnl_2
+LOCAL_STATIC_LIBRARIES := libnl
 LOCAL_MODULE := iw
 
 $(IW_SOURCE_DIR)/version.c:
