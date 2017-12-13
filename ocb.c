f7504581850f21f24376e2df53338770c9c18eb0
Peter Große <pegro@friiks.de>
2017-12-13 13:58:40 +0100

    ocb: Use common freqchan helper for setting the operating channel
    Simplify code by using the helper which has been introduced earlier.
    
    Signed-off-by: Peter Große <pegro@friiks.de>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- ocb.c
+++ ocb.c
@@ -10,56 +10,22 @@ static int join_ocb(struct nl80211_state *state,
 		    struct nl_msg *msg, int argc, char **argv,
 		    enum id_input id)
 {
-	unsigned long freq;
-	char *end;
-	unsigned int i;
-	const struct chanmode *chanmode_selected = NULL;
-	static const struct chanmode chanmode[] = {
-		{ .name = "5MHz",
-		  .width = NL80211_CHAN_WIDTH_5,
-		  .freq1_diff = 0,
-		  .chantype = -1 },
-		{ .name = "10MHz",
-		  .width = NL80211_CHAN_WIDTH_10,
-		  .freq1_diff = 0,
-		  .chantype = -1 },
-	};
+	struct chandef chandef;
+	int err, parsed;
 
 	if (argc < 2)
 		return 1;
 
-	/* freq */
-	freq = strtoul(argv[0], &end, 10);
-	if (*end != '\0')
-		return 1;
+	err = parse_freqchan(&chandef, false, argc, argv, &parsed);
 
-	NLA_PUT_U32(msg, NL80211_ATTR_WIPHY_FREQ, freq);
-	argv++;
-	argc--;
+	if (err)
+		return err;
 
-	/* channel width */
-	for (i = 0; i < ARRAY_SIZE(chanmode); i++) {
-		if (strcasecmp(chanmode[i].name, argv[0]) == 0) {
-			chanmode_selected = &chanmode[i];
-			break;
-		}
-	}
-	if (chanmode_selected) {
-		NLA_PUT_U32(msg, NL80211_ATTR_CHANNEL_WIDTH,
-			    chanmode_selected->width);
-		NLA_PUT_U32(msg, NL80211_ATTR_CENTER_FREQ1,
-			    get_cf1(chanmode_selected, freq));
-
-		argv++;
-		argc--;
-	} else {
-		return 1;
-	}
+	put_chandef(msg, &chandef);
+	if (err)
+		return err;
 
 	return 0;
-
-nla_put_failure:
-	return -ENOBUFS;
 }
 COMMAND(ocb, join, "<freq in MHz> <5MHz|10MHz>",
 	NL80211_CMD_JOIN_OCB, 0, CIB_NETDEV, join_ocb,
