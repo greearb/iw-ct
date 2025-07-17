2f74b3966f49b19b471f3567944fb4b2008d764d
Ben Greear <greearb@candelatech.com>
2025-07-17 10:22:05 -0700

    iw:  Add some missing status code descriptions.
    Found here:
    https://www.wifi-professionals.com/2024/08/ieee-802-11-reason-codes-to-identify-network-issues
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- status.c
+++ status.c
@@ -1,9 +1,18 @@
 #include <stdint.h>
 #include "iw.h"
 
+/* This seems to be a decent reference:
+ * https://www.wifi-professionals.com/2024/08/ieee-802-11-reason-codes-to-identify-network-issues
+ */
+
 static const char *status_table[] = {
 	[0] = "Successful",
 	[1] = "Unspecified failure",
+	[2] = "Tunneled direct link setup (TDLS) wakeup schedule rejected but alternative schedule provided",
+	[3] = "TDLS wakeup schedule rejected",
+	[5] = "Security disabled",
+	[6] = "Unacceptable lifetime",
+	[7] = "Not in same basic service set (BSS)",
 	[10] = "Cannot support all requested capabilities in the capability information field",
 	[11] = "Reassociation denied due to inability to confirm that association exists",
 	[12] = "Association denied due to reason outside the scope of this standard",
@@ -49,6 +58,32 @@ static const char *status_table[] = {
 	[53] = "Invalid PMKID",
 	[54] = "Invalid MDIE",
 	[55] = "Invalid FTIE",
+	[56] = "Requested traffic classification (TCLAS) processing not supported by AP",
+	[57] = "AP has insufficient TCLAS processing resources to satisfy request",
+	[58] = "TS not created because request can’t be honored; however, HC suggests station transitions to other BSSs to set up TS",
+	[59] = "Generic advertisement service (GAS) advertisement protocol not supported",
+	[60] = "No outstanding GAS request",
+	[61] = "GAS response not received from advertisement server",
+	[62] = "Station timed out waiting for GAS query response",
+	[63] = "GAS response larger than query response length limit",
+	[64] = "Request refused because home network doesn’t support request",
+	[65] = "Advertisement server in network not currently reachable",
+	[68] = "Request refused because AP doesn’t support unauthenticated access",
+	[73] = "Unscheduled Automatic Power Save Delivery (U-APSD) coexistence not supported",
+	[74] = "Requested U-APSD coexistence mode not supported",
+	[75] = "Requested interval or duration value can’t be supported with U-APSD coexistence",
+	[76] = "Authentication rejected because anti-clogging token is required",
+	[77] = "Authentication rejected because the offered finite cyclic group not supported",
+	[78] = "The Target Beacon Transmission Time (TBTT) adjustment request not successful because station couldn’t find alternative TBTT",
+	[79] = "Transmission failure",
+	[80] = "Requested TCLAS not supported",
+	[81] = "TCLAS resources exhausted",
+	[82] = "Rejected with suggested BSS transition",
+	[93] = "Association or reassociation refused because of memory limits at AP",
+	[94] = "Association or reassociation refused because emergency services not supported at AP",
+	[95] = "GAS query response not yet received",
+	[101] = "Request failed due to exceeded MCCA access fraction (MAF) limit",
+	[102] = "Request failed due to exceeded MCF coordinated channel access (MCCA) track limit, where MCF refers to mesh coordination function",
 };
 
 const char *get_status_str(uint16_t status)
